0ae84d7581f193b2b9c3c17612c9f9ffc42272ad
Henrique de Souza <enriquedesuu@gmail.com>
2023-10-23 14:57:50 -0300

    atualização de funções

--- README.md
+++
@@ -1,2 +0,0 @@
-# adivinhe_o_numero
-Este joguinho, faz com que o usuário tente adivinhar o número que o computador está "pensando", dando dicas para que ele acerte com o menor número de palpites. No final ele mostra uma mensagem de validação com o total de tentativas até acertar 
--- adivinhe_o_numero.py
+++ adivinhe_o_numero.py
@@ -7,43 +7,102 @@ na tela uma mensagem de validação.
 import emoji
 from random import randint
 from time import sleep
+from colorama import Fore, Style, init
+
+init()
 def linha():
     print('_' * 60)
 
 
-computador = randint(0, 10)
-palpites = 0
+def recomeço():
+    print()
+    print(f'{"ADIVINHAÇÃO":^30}')
+    linha()
+    print(Fore.YELLOW + 'Estou pensando em um número de 0 a 10' + Style.RESET_ALL)
+recomeço()
+
 
 while True:
-    print()
-    print('\033[33mEstou pensando em um número de 0 a 10\033[m')
-    jogador = int(input('Em que número estou pensando?:'))
-    print(f'hmmm... Será que estou pensando no número {jogador}')
-    sleep(3)
-    linha()
-    # Sistema de condições ---------------------------------------------------------------------------------------------
-    if jogador != computador and jogador > computador:
-        print(f'Não pensei no número \033[31m',jogador,'\033[m')
-        print('Menos...')
-        palpites += 1
-        linha()
-    elif jogador != computador and jogador < computador:
-        print(f'Não pensei no número \033[31m', jogador, '\033[m')
-        print('Mais...')
-        palpites += 1
-        linha()
-    else:
-        print(' ')
-        print(f'\033[1;35m👏 PARABÉNS 👏\033[m, eu havia pensando no numero\033[34m',jogador,'\033[m.')
-        palpites += 1
-        break
+    computador = randint(0, 10)
+    palpites = 0
 
-# mensagem final -------------------------------------------------------------------------------------------------------
-if palpites > 1:
-    print(f'Você teve {palpites} palpites até acertar')
-    linha()
-elif palpites == 1:
-    print('Sensacional, acertou com apenas 1 palpite!')
-    linha()
+    while True:
+        jogador = int(input('Em que número estou pensando?:'))
+        print(f'hmmm... Será que estou pensando no número {jogador}')
+        sleep(2)
+        linha()
+        # Sistema de condições -----------------------------------------------------------------------------------------
+        if jogador != computador and jogador > computador:
+            print(f'Não pensei no número {Fore.RED}{jogador}{Style.RESET_ALL}')
+            print('Menos...')
+            sleep(1)
+            palpites += 1
+            linha()
+        elif jogador != computador and jogador < computador:
+            print(f'Não pensei no número {Fore.RED}{jogador}{Style.RESET_ALL}')
+            print('Mais...')
+            sleep(1)
+            linha()
+            palpites += 1
+        else:
+            print()
+            print()
+            print(f'{Fore.GREEN}👏 PARABÉNS 👏{Style.RESET_ALL}, eu havia pensado no número'
+                  f' {Fore.LIGHTGREEN_EX}{jogador}{Style.RESET_ALL}.')
+            palpites += 1
+            print()
+            linha()
+
+            # mensagem final -------------------------------------------------------------------------------------------
+            if palpites > 1:
+                print(f'Você teve {palpites} palpites até acertar')
+                linha()
+
+            elif palpites == 1:
+                print('Sensacional!!!, acertou com apenas 1 palpite!')
+                linha()
+                break
+
+            # Validação ------------------------------------------------------------------------------------------------
+            tentar_de_novo = str(input('Quer tentar de novo [S/N]:')).strip().upper()[0]
+            if tentar_de_novo == 'S':
+                gerando = 'Gerando um novo game...'
+                for letra in gerando:
+                    print(letra, end='', flush=True)
+                    sleep(0.25)
+                print()
+                linha()
+                recomeço()
+                palpites = 0
+                continue
+
+            elif tentar_de_novo == 'N':
+                validacao = str(input(Fore.RED + 'Tem certeza que deseja finalizar o programa? (y/n)' + Style.RESET_ALL))
+                if validacao.lower() != 'y':
+                    linha()
+                    recomeço()
+                    palpites = 0
+                    continue
+                else:
+                    sleep(2)
+                    mensagem = 'Finalizando programa...'
+                    for letra in mensagem:
+                        print(letra, end='', flush=True)
+                        sleep(0.30)
+                    print()
+                    linha()
+                    break
+    print('<< VOLTE SEMPRE >>')
+    break
+
+
+
+
+
+
+
+
+
+
 
 
